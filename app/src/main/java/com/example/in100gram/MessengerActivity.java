3ae1edff4045eea38b4a8c02b6eca725979aa742
Gatoru90 <lagatordima@gmail.com>
2022-04-28 17:23:51 +0300

    Update README.md

--- app/src/main/java/com/example/in100gram/MessengerActivity.java
+++ app/src/main/java/com/example/in100gram/MessengerActivity.java
@@ -5,21 +5,52 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.util.Calendar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MessengerActivity extends AppCompatActivity{
+    String MessegeText;
+    String timestamp;
+    Calendar calendar = Calendar.getInstance();
+    JSONObject Messobj = new JSONObject();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.messenger_main);
 
+        Button SendBtn = findViewById(R.id.button_gchat_send);
+        EditText editMessegeText = findViewById(R.id.edit_gchat_message);
+
         RecyclerView MessengerRecycler = findViewById(R.id.recycler_gchat);
 
         MessengerRecycler.setLayoutManager(new LinearLayoutManager(this));
         MessengerRecycler.setAdapter(new MessengerAdapter());
+
+        SendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MessegeText = editMessegeText.getText().toString();
+                timestamp = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+                try {
+                    Messobj.put("MessageText",MessegeText);
+                    Messobj.put("Timestamp",timestamp);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.d("Messobj", Messobj.toString());
+                editMessegeText.setText("");
+            }
+        });
     }
 }
--- app/src/main/java/com/example/in100gram/RegActivity.java
+++ app/src/main/java/com/example/in100gram/RegActivity.java
@@ -1,25 +1,10 @@
 package com.example.in100gram;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.InputType;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-
 public class RegActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
